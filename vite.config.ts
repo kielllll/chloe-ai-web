d973f09bf391b7481287827d67b1e4d9e7aab139
kielllll <eatarranza@gmail.com>
2026-01-17 00:21:07 +0800

    chore: setup cf worker deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
+import { defineConfig, loadEnv } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-const config = defineConfig({
-  plugins: [
-    devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default config
+  return {
+    plugins: [
+      devtools(),
+      cloudflare({ viteEnvironment: { name: "ssr" } }),
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+      tailwindcss(),
+      tanstackStart(),
+      viteReact(),
+    ],
+    define: {
+      "import.meta.env.VITE_PUBLIC_BE_URL": JSON.stringify(
+        env.VITE_PUBLIC_BE_URL || "http://localhost:3000",
+      ),
+    },
+  };
+});
+
+export default config;
